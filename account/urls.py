dfc92fe1ffaf8e6fd0e2588e1ddbd50002e29118
vagrant <vagrant@precise64.(none)>
2015-01-08 15:01:12 +0000

    python test login and register

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from account import views
+
+ 
+urlpatterns = patterns('',
+    #url(r'^$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$',views.register,name = 'register'),
+)
